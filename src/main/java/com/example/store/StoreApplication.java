aa0ae8c9728d8e687e3e04060ac9522d1c66a897
Trillion2 <trillion.dozen@gmail.com>
2026-07-25 08:42:50 +0700

    test: test git user name

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
-    // test commit
+
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
     }
