4ec09b471c41f838e83330dbef5d97873a6197be
Trillion <trillion.dozen@gmail.com>
2026-07-25 08:35:11 +0700

    test: test new commit

--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
-	}
+    // test commit
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
 
 }
